9eef9a726b4727b00f72c085836b7b82361bf1df
kpgokulkumar <119801271+kpgokulkumar@users.noreply.github.com>
2023-05-06 20:45:30 +0530

    minor edits

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { ReactComponent as brokenStitch } from './brokrnStitch.svg';
+import { ReactComponent as brokenStitch } from './brokenStitch.svg';
 import { ReactComponent as oilStain } from './oilStain.svg';
 import { ReactComponent as sewingDefect } from './sewingDefect.svg';
 // import other icons here
