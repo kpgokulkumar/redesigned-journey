31d1cbf6128f40a15ba81e1ee651f26fdbc2565a
kpgokulkumar <119801271+kpgokulkumar@users.noreply.github.com>
2023-05-06 22:23:08 +0530

    updates

--- index.js
+++ index.js
@@ -1,55 +1,50 @@
+import { ReactComponent as asymmetrical } from './asymmetrical.svg';
 import { ReactComponent as brokenStitch } from './brokenStitch.svg';
+import { ReactComponent as damageHole } from './damageHole.svg';
+import { ReactComponent as fabDefects } from './fabDefects.svg';
+import { ReactComponent as fabStains } from './fabStains.svg';
+import { ReactComponent as handlingStain } from './handlingStain.svg';
+import { ReactComponent as looseStitch } from './looseStitch.svg';
+import { ReactComponent as missNonSewing } from './missNonSewing.svg';
+import { ReactComponent as missingSewing } from './missingSewing.svg';
+import { ReactComponent as needleHole } from './needleHole.svg';
 import { ReactComponent as oilStain } from './oilStain.svg';
+import { ReactComponent as openSeam } from './openSeam.svg';
+import { ReactComponent as others } from './others.svg';
+import { ReactComponent as peeling } from './peeling.svg';
+import { ReactComponent as penGum } from './penGum.svg';
+import { ReactComponent as ppRe } from './ppRe.svg';
 import { ReactComponent as sewingDefect } from './sewingDefect.svg';
-import { ReactComponent as I001 } from './001.svg';
-import { ReactComponent as I002 } from './002.svg';
-import { ReactComponent as I003 } from './003.svg';
-import { ReactComponent as I004 } from './004.svg';
-import { ReactComponent as I005 } from './005.svg';
-import { ReactComponent as I006 } from './006.svg';
-import { ReactComponent as I007 } from './007.svg';
-import { ReactComponent as I008 } from './008.svg';
-import { ReactComponent as I009 } from './009.svg';
-import { ReactComponent as I010 } from './010.svg';
-import { ReactComponent as I011 } from './011.svg';
-import { ReactComponent as I012 } from './012.svg';
-import { ReactComponent as I013 } from './013.svg';
-import { ReactComponent as I014 } from './014.svg';
-import { ReactComponent as I015 } from './015.svg';
-import { ReactComponent as I016 } from './016.svg';
-import { ReactComponent as I017 } from './017.svg';
-import { ReactComponent as I018 } from './018.svg';
-import { ReactComponent as I019 } from './019.svg';
-import { ReactComponent as I020 } from './020.svg';
-import { ReactComponent as I021 } from './021.svg';
-import { ReactComponent as I022 } from './022.svg';
+import { ReactComponent as sewingDefects } from './sewingDefects.svg';
+import { ReactComponent as skipStitch } from './skipStitch.svg';
+import { ReactComponent as specOff } from './specOff.svg';
+import { ReactComponent as styleSize } from './styleSize.svg';
+import { ReactComponent as trimCut } from './trimCut.svg';
 // import other icons here
 
 export const RedJourney = {
+    asymmetrical,
     brokenStitch,
+    damageHole,
+    fabDefects,
+    fabStains,
+    handlingStain,
+    looseStitch,
+    missNonSewing,
+    missingSewing,
+    needleHole,
     oilStain,
+    openSeam,
+    others,
+    peeling,
+    penGum,
+    ppRe,
     sewingDefect,
-    I001,
-    I002,
-    I003,
-    I004,
-    I005,
-    I006,
-    I007,
-    I008,
-    I009,
-    I010,
-    I011,
-    I012,
-    I013,
-    I014,
-    I015,
-    I016,
-    I017,
-    I018,
-    I019,
-    I020,
-    I021,
-    I022
+    sewingDefects,
+    skipStitch,
+    specOff,
+    styleSize,
+    trimCut,
+    uncutLoose
     // add other icons here
 };
