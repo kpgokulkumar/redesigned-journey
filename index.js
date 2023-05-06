f4865de7a938867dab2237a0cdf71df5c5fbb160
kpgokulkumar <119801271+kpgokulkumar@users.noreply.github.com>
2023-05-06 20:43:20 +0530

    icons added for trial

---
+++ index.js
@@ -0,0 +1,12 @@
+import { ReactComponent as brokenStitch } from './brokrnStitch.svg';
+import { ReactComponent as oilStain } from './oilStain.svg';
+import { ReactComponent as sewingDefect } from './sewingDefect.svg';
+// import other icons here
+
+export const RedJourney = {
+    brokenStitch,
+    oilStain,
+    sewingDefect,
+  // add other icons here
+};
+ 
